161b61075ca217b0be05a7dab2b22a68940e77d4
Enrique <enrique.santafe@hotmail.com>
2022-05-18 18:47:29 +0200

    update title

--- src/App.jsx
+++ src/App.jsx
@@ -7,7 +7,7 @@ export const App = () => {
   return (
     <Router>
       <header>
-        <Link to='/'><h1 className={styles.title}>Pelis con palomitas</h1></Link>
+        <Link to='/'><h1 className={styles.title}>🍿🍿 Movies 🍿🍿</h1></Link>
       </header>
       <main>
         <Switch>
